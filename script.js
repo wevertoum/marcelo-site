a205ec0dd9aa0fccdc26432e198a279c97843d2a
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-09-12 14:57:08 -0300

    formatando codigo e limpando uns trem

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-var basicScrollTop = function () {  
+let basicScrollTop = function () {
   // The button
-  var btnTop = document.querySelector('#goTop');
+  let btnTop = document.querySelector("#goTop");
   // Reveal the button
-  var btnReveal = function () { 
-    var position = document.querySelector('#position');
-        position.innerHTML = window.scrollY + 'px';
+  let btnReveal = function () {
+    let position = document.querySelector("#position");
+    position.innerHTML = window.scrollY + "px";
     if (window.scrollY >= 300) {
-      btnTop.classList.add('is-visible');
+      btnTop.classList.add("is-visible");
     } else {
-      btnTop.classList.remove('is-visible');
-    }    
-  }  
+      btnTop.classList.remove("is-visible");
+    }
+  };
   // Smooth scroll top
   // Thanks to => http://stackoverflow.com/a/22610562
-  var TopscrollTo = function () {
-    if(window.scrollY!=0) {
-      setTimeout(function() {
-        window.scrollTo(0,window.scrollY-30);
+  let TopscrollTo = function () {
+    if (window.scrollY != 0) {
+      setTimeout(function () {
+        window.scrollTo(0, window.scrollY - 30);
         TopscrollTo();
       }, 5);
     }
-  }
+  };
   // Listeners
-  window.addEventListener('scroll', btnReveal);
-  btnTop.addEventListener('click', TopscrollTo);  
-    
+  window.addEventListener("scroll", btnReveal);
+  btnTop.addEventListener("click", TopscrollTo);
 };
 basicScrollTop();
