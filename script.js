1a7a2334b73061e38222ed2a3afdaa34538b6f70
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-09-12 15:13:08 -0300

    limpando...

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 let basicScrollTop = function () {
-  // The button
   let btnTop = document.querySelector("#goTop");
-  // Reveal the button
+
   let btnReveal = function () {
-    let position = document.querySelector("#position");
-    position.innerHTML = window.scrollY + "px";
     if (window.scrollY >= 300) {
       btnTop.classList.add("is-visible");
     } else {
       btnTop.classList.remove("is-visible");
     }
   };
-  // Smooth scroll top
-  // Thanks to => http://stackoverflow.com/a/22610562
+
   let TopscrollTo = function () {
     if (window.scrollY != 0) {
       setTimeout(function () {
@@ -21,7 +17,7 @@ let basicScrollTop = function () {
       }, 5);
     }
   };
-  // Listeners
+
   window.addEventListener("scroll", btnReveal);
   btnTop.addEventListener("click", TopscrollTo);
 };
